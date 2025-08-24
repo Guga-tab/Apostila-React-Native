bb0cd1ca45a93f26242ef8f8dba90c600d016bc3
Guga-tab <gustavomachado.521@gmail.com>
2025-08-24 11:43:59 -0300

    Adicionando capítulo 3

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomButtom from './componentes/CustomButtom/CustomButtom';
+import TextInputBox from './componentes/TextInputBox/TextInputBox';
+import FuncaoSoma from './acoes/FuncaoSoma';
 
 export default function App() {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+
   return (
-  <View style={styles.container}>
-    <Text>Open up App.js to start working on your app!</Text>
-    <StatusBar style="auto" />
-  </View>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text style={styles.title}>Soma de Dois Números</Text>
+      <TextInputBox
+        value={number1}
+        onChangeText={setNumber1}
+        placeholder="Digite o primeiro número"
+        keyboardType="numeric"
+      />
+      <TextInputBox
+        value={number2}
+        onChangeText={setNumber2}
+        placeholder="Digite o segundo número"
+        keyboardType="numeric"
+      />
+      <CustomButtom
+        title="Somar"
+        onPress={() => FuncaoSoma(number1,number2)}
+        style={styles.button}
+      />
+    </View>
   );
 }
 
@@ -17,4 +41,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  }
 });
---
+++ acoes/FuncaoSoma.js
@@ -0,0 +1,10 @@
+import { Alert } from 'react-native';
+
+export default function funcaoSoma(number1, number2) {
+    const sum = parseFloat(number1) + parseFloat(number2);
+    if (isNaN(sum)) {
+        Alert.alert('Erro', 'Por favor, insira números válidos.');
+        } else {
+        Alert.alert('Resultado', `A soma é: ${sum}`);
+    }
+}
